4705034529df15184586e0927875a6bca728bc26
riggles1 <tmperma2@gmail.com>
2025-05-01 00:10:50 +0200

    Add files via upload

---
+++ Core/NtscFilter.h
@@ -0,0 +1,30 @@
+#pragma once
+#include "stdafx.h"
+#include "BaseVideoFilter.h"
+#include "../Utilities/nes_ntsc.h"
+
+class Console;
+
+class NtscFilter : public BaseVideoFilter
+{
+private:
+	nes_ntsc_setup_t _ntscSetup;
+	nes_ntsc_t _ntscData;
+	bool _keepVerticalRes = false;
+	bool _useExternalPalette = true;
+	uint8_t _palette[512 * 3];
+	uint32_t* _ntscBuffer;
+	bool _ntscBorder = true;
+
+	void GenerateArgbFrame(uint32_t *outputBuffer);
+
+protected:
+	void OnBeforeApplyFilter();
+
+public:
+	NtscFilter(shared_ptr<Console> console);
+	virtual ~NtscFilter();
+
+	virtual void ApplyFilter(uint16_t *ppuOutputBuffer);
+	virtual FrameInfo GetFrameInfo();
+};
