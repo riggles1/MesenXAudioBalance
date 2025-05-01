dc632e91185e72f4a00ce2b21d5fac3cd79c837a
riggles1 <tmperma2@gmail.com>
2025-05-01 02:04:47 +0200

    Delete Utilities directory

--- Utilities/KreedSaiEagle/SaiEagle.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "../stdafx.h"
-
-extern void supertwoxsai_generic_xrgb8888(unsigned width, unsigned height, uint32_t *src, unsigned src_stride, uint32_t *dst, unsigned dst_stride);
-extern void twoxsai_generic_xrgb8888(unsigned width, unsigned height, uint32_t *src, unsigned src_stride, uint32_t *dst, unsigned dst_stride);
-extern void supereagle_generic_xrgb8888(unsigned width, unsigned height, uint32_t *src, unsigned src_stride, uint32_t *dst, unsigned dst_stride);
-
